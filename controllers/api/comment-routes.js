d880fb1b440ff26c27d08f1ef0f6133a535229ed
pardue95 <85760640+pardue95@users.noreply.github.com>
2021-12-13 16:10:40 -0600

    lesson 1

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
-// get all commnets
 router.get('/', (req, res) => {
   Comment.findAll()
     .then(dbCommentData => res.json(dbCommentData))
@@ -12,27 +11,28 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    Comment.create({
-  comment_text: req.body.comment_text,
-  user_id: req.body.user_id,
-  post_id: req.body.post_id
-})
-  .then(dbCommentData => res.json(dbCommentData))
-  .catch(err => {
-    console.log(err);
-    res.status(400).json(err);
-  });
+  // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
+  Comment.create({
+    comment_text: req.body.comment_text,
+    user_id: req.session.user_id,
+    post_id: req.body.post_id
+  })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
-    Comment.destroy({
+  Comment.destroy({
     where: {
       id: req.params.id
     }
   })
     .then(dbCommentData => {
       if (!dbCommentData) {
-        res.status(404).json({ message: 'No post found with this id' });
+        res.status(404).json({ message: 'No comment found with this id!' });
         return;
       }
       res.json(dbCommentData);
