1943c59ebb3d89d017f1bc9bf630dc3ad9e7441f
pardue95 <85760640+pardue95@users.noreply.github.com>
2021-12-29 18:16:54 -0600

    fix heroku

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     });
 });
-
+//this is a comment
 router.get('/:id', (req, res) => {
   User.findOne({
     attributes: { exclude: ['password'] },
